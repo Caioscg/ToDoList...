e7a49f65602c37708a913bf69f7536a08afc55c2
Caioscg <cacazaok5@gmail.com>
2023-10-26 23:39:22 -0300

    global styles

---
+++ src/styles/global.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.COLORS.DARK_200};
+        color: ${({ theme }) => theme.COLORS.LIGHT_200};
+    }
+
+    body, input, button, textarea {
+        //font-family: 'Poppins', sans-serif;
+        outline: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.87);
+    }
+
+    // react loading spinner: 
+
+    .spinner {
+        display: block;
+        margin: auto;
+    }
+
+    //animations:
+`
