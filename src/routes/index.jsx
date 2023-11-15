a3252e3605e5e658972916568c8fe7332381ce5d
Caioscg <cacazaok5@gmail.com>
2023-11-14 23:49:12 -0300

    enter page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 
 import { AuthRoutes } from "./auth.routes"
+import { AppRoutes } from "./app.routesroutes"
 
 export function Routes() {
     return(
