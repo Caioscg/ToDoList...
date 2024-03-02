a6d4d702f3243deec7aedcc162d3b39acefe653a
Caioscg <cacazaok5@gmail.com>
2024-03-01 23:54:28 -0300

    month background

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
     cursor: pointer;
     width: fit-content;
+
+    padding: 4px;
+    border-radius: 6px;
     
     font-size: 18px;
     font-family: 'Montserrat', sans-serif;
@@ -11,4 +14,9 @@ export const Container = styled.div`
 
     font-weight: ${({ highlight }) => highlight? 700 : 500 };
     color: ${({ theme }) => theme.COLORS.DARK_100};
+    background-color: ${({ theme }) => theme.COLORS.DARK_400};
+
+    :hover {
+        background-color: ${({ theme }) => theme.COLORS.DARK_100};
+    }
 `
